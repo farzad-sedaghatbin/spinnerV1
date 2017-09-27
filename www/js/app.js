c509e04d80fc8fe42ad4361ef9ba0e343e534ac4
omid.javaheri66@gmail.com <omid.javaheri66@gmail.com>
2017-09-27 11:16:16 +0330

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -7,16 +7,8 @@ var app = angular.module('starter', ['ionic','starter.controllers','starter.serv
 
 .run(function($ionicPlatform,$http,$rootScope,$ionicHistory,$timeout) {
   $ionicPlatform.ready(function() {
-    inappbilling.init();
-    inappbilling.consumePurchase(function () {
-    }, function (e) {
-      menuService.myMessage("خرید مجدد این محصول برای شما ممکن نخواهد بود، بدلیل خطای کافه بازار.برای رفع مشکل با ما تماس بگیرید", "خطا");
-    }, "gas")
-    inappbilling.consumePurchase(function () {
-    }, function (e) {
-      menuService.myMessage("خرید مجدد این محصول برای شما ممکن نخواهد بود، بدلیل خطای کافه بازار.برای رفع مشکل با ما تماس بگیرید", "خطا");
-    }, "gem1")
-    tapsell.initialize('rnljdeagkbdqakojgecndcrbbfkgdfpdjqfnhablpjbpghfjsftnchctaqlejblmqdkmga');
+    // inappbilling.init();
+    // tapsell.initialize('rnljdeagkbdqakojgecndcrbbfkgdfpdjqfnhablpjbpghfjsftnchctaqlejblmqdkmga');
     if(window.cordova && window.cordova.plugins.Keyboard) {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
