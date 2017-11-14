36d0d4fdca9566832b44293385055b04da7fe234
omid.javaheri66@gmail.com <omid.javaheri66@gmail.com>
2017-11-14 10:15:09 +0330

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -51,20 +51,20 @@ var app = angular.module('starter', ['ionic', 'starter.controllers', 'starter.se
         navigator.app.exitApp();
 
       }
-      // if(device.isVirtual){
-      //   alert("بر روی شبیه ساز قابلیت اجرا وجود ندارد");
-      //   navigator.app.exitApp();
-      // }
-      // inappbilling.init();
-      // tapsell.initialize('rnljdeagkbdqakojgecndcrbbfkgdfpdjqfnhablpjbpghfjsftnchctaqlejblmqdkmga');
-      // if(!$rootScope.myAudio){
-      //   $rootScope.myAudio = new Media("http://dagala.ir/Era_Ameno.mp3");
-      //   if (!$rootScope.isMute) {
-      //     $rootScope.myAudio.pause();
-      //   }else {
-      //     $rootScope.myAudio.play({numberOfLoops: 9999});
-      //   }
-      // }
+      if(device.isVirtual){
+        alert("بر روی شبیه ساز قابلیت اجرا وجود ندارد");
+        navigator.app.exitApp();
+      }
+      inappbilling.init();
+      tapsell.initialize('rnljdeagkbdqakojgecndcrbbfkgdfpdjqfnhablpjbpghfjsftnchctaqlejblmqdkmga');
+      if(!$rootScope.myAudio){
+        $rootScope.myAudio = new Media("http://dagala.ir/Era_Ameno.mp3");
+        if (!$rootScope.isMute) {
+          $rootScope.myAudio.pause();
+        }else {
+          $rootScope.myAudio.play({numberOfLoops: 9999});
+        }
+      }
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         cordova.plugins.Keyboard.disableScroll(true);
