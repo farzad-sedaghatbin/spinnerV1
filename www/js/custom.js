ff821d3e981738575e21c4029a3fe67fe290533f
farzad <farzad.sedaghatbin@gmail.com>
2017-11-29 17:11:31 +0330

    icon css

--- www/js/custom.js
+++ www/js/custom.js
@@ -2,11 +2,8 @@ function myLoading() {
   var parent = $("<div></div>");
   parent.addClass("omid_spinner");
   var spinner = $("<div></div>");
-  spinner.addClass("myspinner");
   var dot1 = $("<div></div>");
-  dot1.addClass("dot1");
   var dot2 = $("<div></div>");
-  dot2.addClass("dot2");
   parent.append(spinner);
   spinner.append(dot1);
   spinner.append(dot2);
