0cc66ac907ab0a38e394570c63e9235d12f96e28
omid.javaheri66@gmail.com <omid.javaheri66@gmail.com>
2017-12-03 10:17:15 +0330

    ...

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -209,7 +209,7 @@ angular.module('starter.controllers', [])
     }, 700);
 
     $scope.disable = function (id) {
-      return $.inArray(id, menuService.getPlayedGames()) > -1;
+      return true;
     };
     $scope.help = function () {
       menuService.boardHelp();
